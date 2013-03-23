b3c44b6181cff31551a1264f60a3e21aa59539da
Jesse van den Kieboom <jesse.vandenkieboom@epfl.ch>
2013-03-23 12:04:25 +0100

    Fix linking against dl

--- sqlite3_c.go
+++ sqlite3_c.go
@@ -4,9 +4,9 @@ package sqlite
 #cgo CFLAGS: -I.
 #cgo windows CFLAGS:  -fno-stack-check -fno-stack-protector -mno-stack-arg-probe
 #cgo windows LDFLAGS: -lmingwex -lmingw32
-#cgo linux LDFLAGS: -dl
-#cgo freebsd LDFLAGS: -dl
-#cgo netbsd LDFLAGS: -dl
-#cgo openbsd LDFLAGS: -dl
+#cgo linux LDFLAGS: -ldl
+#cgo freebsd LDFLAGS: -ldl
+#cgo netbsd LDFLAGS: -ldl
+#cgo openbsd LDFLAGS: -ldl
 */
 import "C"
