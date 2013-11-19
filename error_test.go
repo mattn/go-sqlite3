72bb737cf97755013eafb2a2ab56d09cdeee3e8b
Robert Knight <robertknight@gmail.com>
2013-11-19 14:05:48 +0000

    Add unit test for enhanced error reporting
    Add a test to check for a useful response for a SQL
    query that cannot be executed due to a constraint failure

--- error_test.go
+++ error_test.go
@@ -8,7 +8,7 @@ import (
 	"testing"
 )
 
-func TestFailures(t *testing.T) {
+func TestCorruptDbErrors(t *testing.T) {
 	dirName, err := ioutil.TempDir("", "sqlite3")
 	if err != nil {
 		t.Fatal(err)
@@ -36,4 +36,29 @@ func TestFailures(t *testing.T) {
 		t.Error("wrong error string for corrupted DB")
 	}
 	db.Close()
+}
+
+func TestSqlLogicErrors(t *testing.T) {
+	dirName, err := ioutil.TempDir("", "sqlite3")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer os.RemoveAll(dirName)
+
+	dbFileName := path.Join(dirName, "test.db")
+	db, err := sql.Open("sqlite3", dbFileName)
+	if err != nil {
+		t.Error(err)
+	}
+
+	_, err = db.Exec("CREATE TABLE Foo (id INT PRIMARY KEY)")
+	if err != nil {
+		t.Error(err)
+	}
+
+	const expectedErr = "table Foo already exists"
+	_, err = db.Exec("CREATE TABLE Foo (id INT PRIMARY KEY)")
+	if err.Error() != expectedErr {
+		t.Errorf("Unexpected error: %s, expected %s", err.Error(), expectedErr)
+	}
 }
