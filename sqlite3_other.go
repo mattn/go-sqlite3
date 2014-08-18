5e5d088a3662a12104630827ac6926f7626d23cf
mattn <mattn.jp@gmail.com>
2014-08-18 16:56:31 +0900

    Add license header

--- sqlite3_other.go
+++ sqlite3_other.go
@@ -1,3 +1,7 @@
+// Copyright (C) 2014 Yasuhiro Matsumoto <mattn.jp@gmail.com>.
+//
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file.
 // +build !windows
 
 package sqlite3
