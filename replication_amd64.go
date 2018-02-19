3500b52b3eb3cff0c306daf6c5ec943cd15eda23
Free Ekanayaka <free.ekanayaka@canonical.com>
2018-02-19 12:55:59 +0000

    Reduce max number of WAL pages that can be sent at once

--- replication_amd64.go
+++ replication_amd64.go
@@ -5,5 +5,5 @@ package sqlite3
 import "unsafe"
 
 func unsafePointerToSlice(pList unsafe.Pointer, n int) []ReplicationPage {
-	return (*[1 << 30]ReplicationPage)(pList)[:n:n]
+	return (*[1 << 16]ReplicationPage)(pList)[:n:n]
 }
