bdda6e10e3e81e0e81c629d80fd384f680cbdf67
Gert-Jan Timmer <gjr.timmer@gmail.com>
2018-05-23 22:21:11 +0200

    Add: SQLITE_INTROSPECTION_PRAGMAS

---
+++ sqlite3_opt_introspect.go
@@ -0,0 +1,13 @@
+// Copyright (C) 2014 Yasuhiro Matsumoto <mattn.jp@gmail.com>.
+//
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file.
+// +build introspect
+
+package sqlite3
+
+/*
+#cgo CFLAGS: -DSQLITE_INTROSPECTION_PRAGMAS
+#cgo LDFLAGS: -lm
+*/
+import "C"
