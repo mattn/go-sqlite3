de217e4b87906a1f5ee05d43cd8f5ee1dbf40c62
Gert-Jan Timmer <gjr.timmer@gmail.com>
2018-05-23 22:20:35 +0200

    Add: SQLITE_ALLOW_URI_AUTHORITY

---
+++ sqlite3_opt_allow_uri_authority.go
@@ -0,0 +1,13 @@
+// Copyright (C) 2014 Yasuhiro Matsumoto <mattn.jp@gmail.com>.
+//
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file.
+// +build allow_uri_authority
+
+package sqlite3
+
+/*
+#cgo CFLAGS: -DSQLITE_ALLOW_URI_AUTHORITY
+#cgo LDFLAGS: -lm
+*/
+import "C"
