ebb175735b944bed52645590d75909a2d3ca8b74
Gert-Jan Timmer <gjr.timmer@gmail.com>
2018-05-23 22:21:51 +0200

    Add: SQLITE_DEFAULT_AUTOVACUUM

---
+++ sqlite3_opt_vacuum_full.go
@@ -0,0 +1,13 @@
+// Copyright (C) 2014 Yasuhiro Matsumoto <mattn.jp@gmail.com>.
+//
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file.
+// +build vacuum_full
+
+package sqlite3
+
+/*
+#cgo CFLAGS: -DSQLITE_DEFAULT_AUTOVACUUM=1
+#cgo LDFLAGS: -lm
+*/
+import "C"
