edb95b5914045f55e676a85f84446841e17302c0
Gert-Jan Timmer <gjr.timmer@gmail.com>
2018-05-25 14:15:46 +0200

    Add: Initial usleep option
    [ci skip]

---
+++ sqlite3_opt_usleep.go
@@ -0,0 +1,13 @@
+// +build cgo
+// +build windows
+
+// Copyright (C) 2018 G.J.R. Timmer <gjr.timmer@gmail.com>.
+//
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file.
+
+package sqlite3
+
+/*
+
+/*
