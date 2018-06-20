6dc5ec6aa3ff2504ebf13c7c3767724c39171263
Gert-Jan Timmer <gjr.timmer@gmail.com>
2018-06-20 16:33:38 +0200

    Add backup

--- driver/backup_test.go
+++ driver/backup_test.go
@@ -1,5 +1,9 @@
+// Copyright (C) 2018 The Go-SQLite3 Authors.
+//
 // Use of this source code is governed by an MIT-style
 // license that can be found in the LICENSE file.
+
+// +build cgo
 
 package sqlite3
 
