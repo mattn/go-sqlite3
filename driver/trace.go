79fc5c45816f9471d187cb709bfb88fc0abf0b20
Gert-Jan Timmer <gjr.timmer@gmail.com>
2018-06-20 17:02:01 +0200

    Add trace

--- driver/trace.go
+++ driver/trace.go
@@ -1,4 +1,4 @@
-// Copyright (C) 2016 Yasuhiro Matsumoto <mattn.jp@gmail.com>.
+// Copyright (C) 2018 The Go-SQLite3 Authors.
 //
 // Use of this source code is governed by an MIT-style
 // license that can be found in the LICENSE file.
