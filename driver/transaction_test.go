ded182737b4f400cadf9c51e0204369413ee1365
Gert-Jan Timmer <gjr.timmer@gmail.com>
2018-06-27 16:43:15 +0200

    Add Tests
    * Add TestTransactionConn
    
    #598

--- driver/transaction_test.go
+++ driver/transaction_test.go
@@ -9,6 +9,7 @@ package sqlite3
 
 import (
 	"database/sql"
+	"fmt"
 	"os"
 	"testing"
 )
@@ -69,5 +70,40 @@ func TestTransaction(t *testing.T) {
 	rows, err = tx.Query("SELECT id from foo")
 	if err == nil {
 		t.Fatal("Expected failure to query")
+	}
+}
+
+func TestTransactionConn(t *testing.T) {
+	tempFilename := TempFilename(t)
+	defer os.Remove(tempFilename)
+
+	driverName := fmt.Sprintf("sqlite3.%s", t.Name())
+
+	// The driver's connection will be needed in order to perform the backup.
+	var dbConn *SQLiteConn
+	sql.Register(driverName, &SQLiteDriver{
+		ConnectHook: func(conn *SQLiteConn) error {
+			dbConn = conn
+			return nil
+		},
+	})
+
+	db, err := sql.Open(driverName, tempFilename)
+	if err != nil {
+		t.Fatal("Failed to open database:", err)
+	}
+	defer db.Close()
+
+	_, err = db.Exec("CREATE TABLE foo(id INTEGER)")
+	if err != nil {
+		t.Fatal("Failed to create table:", err)
+	}
+
+	tx, err := dbConn.Begin()
+	if err != nil {
+		t.Fatal(err)
+	}
+	if tx == nil {
+		t.Fatal("Failed to create Transaction")
 	}
 }
