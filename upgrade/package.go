d2d0bf1bf1b780cd7cf73de96519d45f50bc47f9
Mario Trangoni <mjtrangoni@gmail.com>
2018-11-21 11:28:08 +0100

    upgrade/package.go: Fix golint issue

--- upgrade/package.go
+++ upgrade/package.go
@@ -1,6 +1,4 @@
-// Package upgrade
-//
-// Dummy to ensure package can be loaded
+// Package upgrade is a dummy package to ensure package can be loaded
 //
 // This file is to avoid the following error:
 // can't load package: package go-sqlite3/upgrade: build constraints exclude all Go files in go-sqlite3\upgrade
