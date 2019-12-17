53cff3fcebd1e177d04129dc131523635bc45d3d
rittneje <rittneje@gmail.com>
2019-12-17 02:07:49 -0500

    fix typo in doc comment (#770)

--- sqlite3.go
+++ sqlite3.go
@@ -913,7 +913,7 @@ func (c *SQLiteConn) begin(ctx context.Context) (driver.Tx, error) {
 //      - rwc
 //      - memory
 //
-//   shared
+//   cache
 //     SQLite Shared-Cache Mode
 //     https://www.sqlite.org/sharedcache.html
 //     Values:
@@ -1910,7 +1910,7 @@ func (s *SQLiteStmt) exec(ctx context.Context, args []namedValue) (driver.Result
 		resultCh <- result{r, err}
 	}()
 	select {
-	case rv := <- resultCh:
+	case rv := <-resultCh:
 		return rv.r, rv.err
 	case <-ctx.Done():
 		select {
@@ -2011,7 +2011,7 @@ func (rc *SQLiteRows) Next(dest []driver.Value) error {
 		resultCh <- rc.nextSyncLocked(dest)
 	}()
 	select {
-	case err := <- resultCh:
+	case err := <-resultCh:
 		return err
 	case <-rc.ctx.Done():
 		select {
