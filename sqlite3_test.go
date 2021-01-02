4823017638531c84e67f18895fd87de799c78b05
Philip O'Toole <philip.otoole@yahoo.com>
2021-01-01 22:06:37 -0500

    Export sqlite3_column_table_name

--- sqlite3_test.go
+++ sqlite3_test.go
@@ -1604,6 +1604,40 @@ func TestDeclTypes(t *testing.T) {
 	}
 }
 
+func TestColumnTableName(t *testing.T) {
+        d := SQLiteDriver{}
+        conn, err := d.Open(":memory:")
+        if err != nil {
+                t.Fatal("failed to get database connection:", err)
+        }
+        defer conn.Close()
+        sqlite3conn := conn.(*SQLiteConn)
+
+        _, err = sqlite3conn.Exec(`CREATE TABLE foo (name string)`, nil)
+        if err != nil {
+                t.Fatal("Failed to create table:", err)
+        }
+        _, err = sqlite3conn.Exec(`CREATE TABLE bar (name string)`, nil)
+        if err != nil {
+                t.Fatal("Failed to create table:", err)
+        }
+
+        stmt, err := sqlite3conn.Prepare(`SELECT * FROM foo JOIN bar ON foo.name = bar.name`)
+        if err != nil {
+                t.Fatal(err)
+        }
+
+        if exp, got := "foo", stmt.(*SQLiteStmt).ColumnTableName(0); exp != got {
+                t.Fatalf("Incorrect table name returned expected: %s, got: %s", exp, got)
+        }
+        if exp, got := "bar", stmt.(*SQLiteStmt).ColumnTableName(1); exp != got {
+                t.Fatalf("Incorrect table name returned expected: %s, got: %s", exp, got)
+        }
+        if exp, got := "", stmt.(*SQLiteStmt).ColumnTableName(2); exp != got {
+                t.Fatalf("Incorrect table name returned expected: %s, got: %s", exp, got)
+        }
+}
+
 func TestPinger(t *testing.T) {
 	db, err := sql.Open("sqlite3", ":memory:")
 	if err != nil {
