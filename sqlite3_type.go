cadffb56f6d316543b7100ab6ed312a5bf341fb6
Auler <Auler@users.noreply.github.com>
2021-01-22 15:04:33 +0800

    Update sqlite3_type.go
    Update 'ColumnTypeScanType' method,
    Different types of mapping values

--- sqlite3_type.go
+++ sqlite3_type.go
@@ -1,8 +1,3 @@
-// Copyright (C) 2019 Yasuhiro Matsumoto <mattn.jp@gmail.com>.
-//
-// Use of this source code is governed by an MIT-style
-// license that can be found in the LICENSE file.
-
 package sqlite3
 
 /*
@@ -14,8 +9,9 @@ package sqlite3
 */
 import "C"
 import (
+	"database/sql"
 	"reflect"
-	"time"
+	"strings"
 )
 
 // ColumnTypeDatabaseTypeName implement RowsColumnTypeDatabaseTypeName.
@@ -31,26 +27,78 @@ func (rc *SQLiteRows) ColumnTypeLength(index int) (length int64, ok bool) {
 func (rc *SQLiteRows) ColumnTypePrecisionScale(index int) (precision, scale int64, ok bool) {
 	return 0, 0, false
 }
+*/
 
 // ColumnTypeNullable implement RowsColumnTypeNullable.
 func (rc *SQLiteRows) ColumnTypeNullable(i int) (nullable, ok bool) {
-	return false, false
-}
-*/
-// ColumnTypeScanType implement RowsColumnTypeScanType.
-func (rc *SQLiteRows) ColumnTypeScanType(i int) reflect.Type {
-	switch C.GoString(C.sqlite3_column_decltype(rc.s.s, C.int(i))) {
-	case "int", "integer", "tinyint", "smallint", "mediumint", "bigint":
-		return reflect.TypeOf(int64(0))
-	case "timestamp", "datetime", "date":
-		return reflect.TypeOf(time.Time{})
-	case "boolean":
-		return reflect.TypeOf(false)
-	case "double", "float":
-		return reflect.TypeOf(float64(0))
-	case "varchar", "character", "nchar":
-		return reflect.TypeOf("")
-	}
-	return reflect.SliceOf(reflect.TypeOf(byte(0)))
+	return true, true
 }
 
+// ColumnTypeScanType implement RowsColumnTypeScanType.
+func (rc *SQLiteRows) ColumnTypeScanType(i int) reflect.Type {
+	//ct := C.sqlite3_column_type(rc.s.s, C.int(i))  // Always returns 5
+	return scanType(C.GoString(C.sqlite3_column_decltype(rc.s.s, C.int(i))))
+}
+
+const (
+	SQLITE_INTEGER = iota
+	SQLITE_TEXT
+	SQLITE_BLOB
+	SQLITE_REAL
+	SQLITE_NUMERIC
+	SQLITE_TIME
+	SQLITE_BOOL
+	SQLITE_NULL
+)
+
+func scanType(cdt string) reflect.Type {
+	t := strings.ToUpper(cdt)
+	i := databaseTypeConvSqlite(t)
+	switch i {
+	case SQLITE_INTEGER:
+		return reflect.TypeOf(sql.NullInt64{})
+	case SQLITE_TEXT:
+		return reflect.TypeOf(sql.NullString{})
+	case SQLITE_BLOB:
+		return reflect.TypeOf(sql.RawBytes{})
+	case SQLITE_REAL:
+		return reflect.TypeOf(sql.NullFloat64{})
+	case SQLITE_NUMERIC:
+		return reflect.TypeOf(sql.NullFloat64{})
+	case SQLITE_BOOL:
+		return reflect.TypeOf(sql.NullBool{})
+	case SQLITE_TIME:
+		return reflect.TypeOf(sql.NullTime{})
+	}
+	return reflect.TypeOf(new(interface{}))
+}
+
+func databaseTypeConvSqlite(t string) int {
+	if strings.Contains(t, "INT") {
+		return SQLITE_INTEGER
+	}
+	if t == "CLOB" || t == "TEXT" ||
+		strings.Contains(t, "CHAR") {
+		return SQLITE_TEXT
+	}
+	if t == "BLOB" {
+		return SQLITE_BLOB
+	}
+	if t == "REAL" || t == "FLOAT" ||
+		strings.Contains(t, "DOUBLE") {
+		return SQLITE_REAL
+	}
+	if t == "DATE" || t == "DATETIME" ||
+		t == "TIMESTAMP" {
+		return SQLITE_TIME
+	}
+	if t == "NUMERIC" ||
+		strings.Contains(t, "DECIMAL") {
+		return SQLITE_NUMERIC
+	}
+	if t == "BOOLEAN" {
+		return SQLITE_BOOL
+	}
+
+	return SQLITE_NULL
+}
