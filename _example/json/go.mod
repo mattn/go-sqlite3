5131ffb8eb571c63d1e7a04004ab8aedc05e25cb
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2022-05-28 23:21:23 +0900

    add _example/json

---
+++ _example/json/go.mod
@@ -0,0 +1,29 @@
+module github.com/mattn/go-sqlite3/_example/json
+
+go 1.17
+
+replace github.com/mattn/go-sqlite3 => ../..
+
+require (
+	github.com/apache/thrift v0.0.0-20181112125854-24918abba929 // indirect
+	github.com/goccy/go-json v0.7.6 // indirect
+	github.com/golang/snappy v0.0.0-20180518054509-2e65f85255db // indirect
+	github.com/google/go-cmp v0.4.0 // indirect
+	github.com/guptarohit/asciigraph v0.5.1 // indirect
+	github.com/juju/clock v0.0.0-20190205081909-9c5c9712527c // indirect
+	github.com/juju/errors v0.0.0-20200330140219-3fe23663418f // indirect
+	github.com/juju/loggo v0.0.0-20200526014432-9ce3a2e09b5e // indirect
+	github.com/juju/utils/v2 v2.0.0-20200923005554-4646bfea2ef1 // indirect
+	github.com/klauspost/compress v1.9.7 // indirect
+	github.com/mattn/go-runewidth v0.0.7 // indirect
+	github.com/mattn/go-sqlite3 v1.14.13 // indirect
+	github.com/olekukonko/tablewriter v0.0.4 // indirect
+	github.com/rocketlaunchr/dataframe-go v0.0.0-20211025052708-a1030444159b // indirect
+	github.com/rocketlaunchr/mysql-go v1.1.3 // indirect
+	github.com/xitongsys/parquet-go v1.5.2 // indirect
+	golang.org/x/crypto v0.0.0-20200820211705-5c72a883971a // indirect
+	golang.org/x/exp v0.0.0-20200331195152-e8c3332aa8e5 // indirect
+	golang.org/x/net v0.0.0-20200904194848-62affa334b73 // indirect
+	golang.org/x/sync v0.0.0-20200317015054-43a5402ce75a // indirect
+	gopkg.in/yaml.v2 v2.3.0 // indirect
+)
---
+++ _example/json/main.go
@@ -0,0 +1,110 @@
+package main
+
+import (
+	"context"
+	"database/sql"
+	_ "embed"
+	"fmt"
+	"log"
+	"strings"
+
+	_ "github.com/mattn/go-sqlite3"
+	"github.com/rocketlaunchr/dataframe-go"
+	"github.com/rocketlaunchr/dataframe-go/imports"
+)
+
+//go:embed iris.csv
+var irisData string
+
+func main() {
+	db, err := sql.Open("sqlite3", ":memory:")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	sqlStmt := `
+    create table iris (
+           sepal_length float,
+           sepal_width  float,
+           petal_length float,
+           petal_width  float,
+           class int);
+	`
+	_, err = db.Exec(sqlStmt)
+	if err != nil {
+		log.Printf("%q: %s\n", err, sqlStmt)
+		return
+	}
+
+	tx, err := db.Begin()
+	if err != nil {
+		log.Fatal(err)
+	}
+	stmt, err := tx.Prepare(`
+    insert into iris(
+        sepal_length, sepal_width, petal_length, petal_width, class)
+        values(?, ?, ?, ?, ?)
+    `)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer stmt.Close()
+
+	df, err := imports.LoadFromCSV(context.Background(), strings.NewReader(irisData))
+	if err != nil {
+		log.Fatal(err)
+	}
+	it := df.ValuesIterator(dataframe.ValuesOptions{
+		InitialRow:   0,
+		Step:         1,
+		DontReadLock: true,
+	})
+
+	df.Lock()
+	for {
+		row, vals, _ := it()
+		if row == nil {
+			break
+		}
+		_, err = stmt.Exec(
+			vals["sepal_length"],
+			vals["sepal_width"],
+			vals["petal_length"],
+			vals["petal_width"],
+			vals["class"],
+		)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+	df.Unlock()
+	tx.Commit()
+
+	rows, err := db.Query(`select
+        json_object(
+            'sepal_length', sepal_length,
+			'sepal_width', sepal_width,
+			'petal_length', petal_length,
+			'petal_width', petal_width,
+            'class', class
+        )
+    from iris
+    `)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer rows.Close()
+	for rows.Next() {
+		var r string
+		err = rows.Scan(&r)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println(r)
+	}
+	err = rows.Err()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
