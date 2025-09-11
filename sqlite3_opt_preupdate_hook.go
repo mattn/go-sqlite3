920b60a639c4927753cd35a830555a79dedb22f9
Philip O'Toole <philip.otoole@yahoo.com>
2025-09-10 23:32:44 -0400

    Use GoStringN to deal with embedded nulls

--- sqlite3_opt_preupdate_hook.go
+++ sqlite3_opt_preupdate_hook.go
@@ -75,8 +75,9 @@ func (d *SQLitePreUpdateData) row(dest []any, new bool) error {
 			blobptr := C.sqlite3_value_blob(val)
 			src = C.GoBytes(blobptr, len)
 		case C.SQLITE_TEXT:
+			len := C.sqlite3_value_bytes(val)
 			cstrptr := unsafe.Pointer(C.sqlite3_value_text(val))
-			src = C.GoString((*C.char)(cstrptr))
+			src = C.GoStringN((*C.char)(cstrptr), len)
 		case C.SQLITE_NULL:
 			src = nil
 		}
