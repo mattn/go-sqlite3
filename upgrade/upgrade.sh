8366a001c1c4d13979f74d1fd370ad6c36e91c46
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2026-03-04 09:14:31 +0900

    create pull-request

--- upgrade/upgrade.sh
+++ upgrade/upgrade.sh
@@ -17,4 +17,13 @@ git checkout -b "sqlite-amalgamation-$VERSION"
 git commit -m "Upgrade SQLite to version $VERSION" sqlite3-binding.c sqlite3-binding.h sqlite3ext.h
 git push origin HEAD
 
-gh pr create --title "Upgrade SQLite to version $VERSION" --body "Automated SQLite upgrade to version $VERSION" --web
+MAJOR=$(echo $VERSION | cut -c1-3)
+MINOR=$(echo $VERSION | cut -c4-5)
+PATCH=$(echo $VERSION | cut -c6-7)
+VERSION_STR="${MAJOR}.${MINOR}.${PATCH}"
+CHANGELOG_URL="https://www.sqlite.org/releaselog/${VERSION_STR}.html"
+
+gh pr create --title "Upgrade SQLite to version $VERSION" --body "Automated SQLite upgrade to version $VERSION
+
+## Changelog
+See: $CHANGELOG_URL" --web
